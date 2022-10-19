715d2fbb6b956bfcbe04a6124f88f5fbe68006d6
Gleyson Canindé do Rosario <103382615+Gln1990@users.noreply.github.com>
2022-10-19 17:37:26 -0300

    Add files via upload

---
+++ API_DadosPessoais/API_DadosPessoais/DTO/ImcDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API_DadosPessoais.DTO
+{
+    public class ImcDTO
+    {
+        public string Nome { get; set; }
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Resultado { get; set; }
+        public string Classificacao { get; set; }
+    }
+}
