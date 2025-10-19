d2afbf4a2f2fad6f6350229600a167670130405d
Gleyson Canindé do Rosario <gleyson01rosario@gmail.com>
2025-10-19 19:32:01 -0300

    Delete Scripts directory

--- Scripts/teste.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.galeria-dinamiqa li').click(function(){
-      $('.galeria-dinamiqa li').removeClass("activeItemdinamiqo");
-      $(this).addClass("activeItemdinamiqo");
-    });
-  });
